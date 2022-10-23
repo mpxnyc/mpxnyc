fefde7bfc0ea8a41a91316c829b6c194d884552d
Sudipta Saha <sudipta.bba@gmail.com>
2022-10-23 17:56:18 -0400

    Updated community districts
    New shapefile, and functions to convert between community districts and other spatial units

--- R/convert_spatial_unit_ny.R
+++ R/convert_spatial_unit_ny.R
@@ -1,27 +1,32 @@
 #' Convert Spatial Units in New York City
 #'
-#' @description Convert to parent spatial unit in hierarchy: borough > neighborhood > census_tract
+#' @description Convert to parent spatial unit in hierarchy: borough > community > neighborhood > census_tract
 #'
-#' @param input_census_tract Vector (character) of input census tracts to convert to neighborhoods or boroughs
-#' @param input_neighborhood_name Vector (character) of input neighborhoods to convert to boroughs
-#' @param convert_to String showing how to convert census_tracts. Options are "neighborhood" (default) and "borough"
+#' @param input_census_tract Vector (character) of input census tracts to convert to neighborhoods or boroughs or community districts
+#' @param input_neighborhood_name Vector (character) of input neighborhoods to convert to neighborhoods or boroughs
+#' @param input_community_district Vector (character) of input communities to convert to boroughs
+#' @param convert_to String showing how to convert census_tracts. Options are "neighborhood" (default), "community", and "borough"
 #'
 #' @return Vector (character) of neighborhood or borrough names
 #' @export
 #'
-convert_spatial_unit_ny <- function(input_census_tract = NULL, input_neighborhood_name = NULL, convert_to="neighborhood") {
+convert_spatial_unit_ny <- function(input_census_tract = NULL, input_neighborhood_name = NULL,
+                                    input_community_district = NULL, convert_to="neighborhood") {
 
-  if (!is.null(input_census_tract) & !is.null(input_neighborhood_name)) stop("Use input parameter census_tract or neighborhood_name, not both!")
+  if (sum(is.null(input_census_tract), is.null(input_neighborhood_name), is.null(input_community_district)) > 1) stop("Use only one input paramter!")
 
-  if (is.null(input_census_tract) & is.null(input_neighborhood_name)) stop("Use input parameter census_tract or neighborhood_name.")
-
+  if (is.null(input_census_tract) & is.null(input_neighborhood_name) & is.null(input_community_district)) stop("Use input parameter census_tract or neighborhood_name or community_district.")
 
   if (!is.null(input_census_tract)) {
     if (convert_to == "neighborhood") return(census_to_nbd(input_census_tract))
+    if (convert_to == "community")      return(census_to_cmnty(input_census_tract))
     if (convert_to == "borough")      return(census_to_boro(input_census_tract))
     print("inside")
+  } else if (!is.null(input_neighborhood_name)) {
+    if (convert_to == "community")      return(nbd_to_cmnty(input_neighborhood_name))
+    if (convert_to == "borough")      return(nbd_to_boro(input_neighborhood_name))
   } else {
-    return(nbd_to_boro(input_neighborhood_name))
+    return(cmnty_to_boro(input_community_district))
   }
 
   }
@@ -33,12 +38,23 @@ census_to_nbd <- function(input_census_tract) {
   result
 }
 
-
 census_to_boro <- function(input_census_tract) {
   result <- census_tract_to_boro_vec[input_census_tract]
   names(result) <- NULL
   result
   }
+
+census_to_cmnty <- function(input_census_tract) {
+  result <- census_tract_to_cmnty_vec[input_census_tract]
+  names(result) <- NULL
+  result
+}
+
+nbd_to_cmnty <- function(input_community_name) {
+  result <- census_tract_to_cmnty_vec[input_census_tract]
+  names(result) <- NULL
+  result
+}
 
 nbd_to_boro <- function(input_neighborhood) {
   result <- nbd_to_boro_vec[input_neighborhood]
@@ -46,4 +62,8 @@ nbd_to_boro <- function(input_neighborhood) {
   result
 }
 
-
+cmnty_to_boro <- function(input_community_name) {
+  result <- cmnty_to_boro_vec[input_community_name]
+  names(result) <- NULL
+  result
+}
--- data-raw/create_package_data.R
+++ data-raw/create_package_data.R
@@ -28,11 +28,23 @@ names(census_tract_to_cmnty_vec)  <- census_tract_to_boro_nbd_data$census_tract
 census_tract_to_boro_vec         <- census_tract_to_boro_nbd_data$borough
 names(census_tract_to_boro_vec)  <- census_tract_to_boro_nbd_data$census_tract
 
+nbd_to_cmnty_data <- census_tract_to_boro_nbd_data[,c("neighborhood", "community")] |>
+  unique(by = "neighborhood")
+
+nbd_to_cmnty_vec               <- nbd_to_cmnty_data$community
+names(nbd_to_cmnty_vec)        <- nbd_to_cmnty_data$neighborhood
+
 nbd_to_boro_data <- census_tract_to_boro_nbd_data[,c("neighborhood", "borough")] |>
   unique(by = "neighborhood")
 
 nbd_to_boro_vec               <- nbd_to_boro_data$borough
 names(nbd_to_boro_vec)        <- nbd_to_boro_data$neighborhood
+
+cmnty_to_boro_data <- census_tract_to_boro_nbd_data[,c("community", "borough")] |>
+  unique(by = "community")
+
+cmnty_to_boro_vec               <- cmnty_to_boro_data$borough
+names(cmnty_to_boro_vec)        <- cmnty_to_boro_data$neighborhood
 
 
 borough_sf_obj <- sf::st_read("/data-raw/nybb_22b/nybb.shp") |>
@@ -53,6 +65,7 @@ usethis::use_data(neighborhood_sf_obj, neighborhood_sf_obj,                     
 usethis::use_data(borough_sf_obj, borough_sf_obj,                                                     internal=FALSE, overwrite=TRUE)
 usethis::use_data(community_sf_obj, community_sf_obj,                                                 internal=FALSE, overwrite=TRUE)
 usethis::use_data(census_tract_centroids, census_tract_to_nbd_vec,
-                  census_tract_to_boro_vec, census_tract_to_cmnty_vec, nbd_to_boro_vec,               internal=TRUE,  overwrite=TRUE)
+                  census_tract_to_boro_vec, census_tract_to_cmnty_vec,
+                  nbd_to_cmnty_vec, nbd_to_boro_vec, cmnty_to_boro_vec,                               internal=TRUE,  overwrite=TRUE)
 
 
