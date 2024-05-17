cc6dbc1c0ae81fb77e319fc1e1f0e766ddf1b665
Keletso Makofane <keletso@ceb-admin-325.lan>
2024-05-17 13:53:36 -0400

    updated data

--- data-raw/create_package_data.R
+++ data-raw/create_package_data.R
@@ -1,97 +1,106 @@
-census_tract_sf_obj <- tigris::tracts(state="NY", cb = TRUE, year=2020) |>
-  rbind(tigris::tracts(state="NJ", cb = TRUE, year=2020)) |>
-  dplyr::select(GEOID, NAMELSADCO, STATE_NAME) |>
-  dplyr::rename(census_tract = GEOID, county = NAMELSADCO, state = STATE_NAME)
+# PREP PACKAGE DATA
+## CENSUS TRACT SF OBJECTS
 
-census_tract_centroids <- census_tract_sf_obj |>
-  sf::st_centroid() |>
-  dplyr::select(census_tract)  |>
-  (function(.) dplyr::mutate(., coord = sf::st_coordinates(.)))() |>
-  data.frame() |>
-  (function(.) dplyr::mutate(., lon=coord[,1], lat=coord[,2]))() |>
-  (function(.) dplyr::select(., -c(geometry, coord)))()
+census_tract_sf_obj                 <- tigris::tracts(state="NY", cb = TRUE, year=2020) |>
+                                            rbind(tigris::tracts(state="NJ", cb = TRUE, year=2020)) |>
+                                            dplyr::select(GEOID, NAMELSADCO, STATE_NAME) |>
+                                            dplyr::rename(census_tract = GEOID, county = NAMELSADCO, state = STATE_NAME)
 
-
-census_tract_to_boro_nbd_data <- readr::read_csv("data-raw/nyc2020census_tract_nta_cdta_relationships.csv") |>
-  dplyr::mutate(GEOID = as.character(GEOID)) |>
-  dplyr::select(GEOID, BoroName, NTAName, CDTACode) |>
-  dplyr::rename(census_tract = GEOID, borough = BoroName, neighborhood = NTAName, community = CDTACode) |>
-  data.table::data.table() |>
-  data.table::setkey(census_tract)
-
-census_tract_to_nbd_vec         <- census_tract_to_boro_nbd_data$neighborhood
-names(census_tract_to_nbd_vec)  <- census_tract_to_boro_nbd_data$census_tract
-
-census_tract_to_cmnty_vec         <- census_tract_to_boro_nbd_data$community
-names(census_tract_to_cmnty_vec)  <- census_tract_to_boro_nbd_data$census_tract
-
-census_tract_to_boro_vec         <- census_tract_to_boro_nbd_data$borough
-names(census_tract_to_boro_vec)  <- census_tract_to_boro_nbd_data$census_tract
-
-nbd_to_cmnty_data <- census_tract_to_boro_nbd_data[,c("neighborhood", "community")] |>
-  unique(by = "neighborhood")
-
-nbd_to_cmnty_vec               <- nbd_to_cmnty_data$community
-names(nbd_to_cmnty_vec)        <- nbd_to_cmnty_data$neighborhood
-
-nbd_to_boro_data <- census_tract_to_boro_nbd_data[,c("neighborhood", "borough")] |>
-  unique(by = "neighborhood")
-
-nbd_to_boro_vec               <- nbd_to_boro_data$borough
-names(nbd_to_boro_vec)        <- nbd_to_boro_data$neighborhood
-
-cmnty_to_boro_data <- census_tract_to_boro_nbd_data[,c("community", "borough")] |>
-  unique(by = "community")
-
-cmnty_to_boro_vec               <- cmnty_to_boro_data$borough
-names(cmnty_to_boro_vec)        <- cmnty_to_boro_data$community
+census_tract_centroids              <- census_tract_sf_obj |>
+                                            sf::st_centroid() |>
+                                            dplyr::select(census_tract)  |>
+                                            (function(.) dplyr::mutate(., coord = sf::st_coordinates(.)))() |>
+                                            data.frame() |>
+                                            (function(.) dplyr::mutate(., lon=coord[,1], lat=coord[,2]))() |>
+                                            (function(.) dplyr::select(., -c(geometry, coord)))()
 
 
-borough_sf_obj <- sf::st_read("data-raw/nybb_22b/nybb.shp") |>
-  dplyr::select(BoroName, geometry) |>
-  dplyr::rename(borough = BoroName)
+## DERIVATIVE SF OBJECTS
+census_tract_to_boro_nbd_data       <- readr::read_csv("data-raw/nyc2020census_tract_nta_cdta_relationships.csv") |>
+                                            dplyr::mutate(GEOID = as.character(GEOID)) |>
+                                            dplyr::select(GEOID, BoroName, NTAName, CDTACode) |>
+                                            dplyr::rename(census_tract = GEOID, borough = BoroName, neighborhood = NTAName, community = CDTACode) |>
+                                            data.table::data.table() |>
+                                            data.table::setkey(census_tract)
 
-borough_sf_obj <- sf::st_simplify(borough_sf_obj,
-                                  preserveTopology = T,
-                                  dTolerance = 200)
+census_tract_to_nbd_vec             <- census_tract_to_boro_nbd_data$neighborhood
+names(census_tract_to_nbd_vec)      <- census_tract_to_boro_nbd_data$census_tract
 
-neighborhood_sf_obj  <- sf::st_read("data-raw/NYC_NTA_shp") |>
-  dplyr::select(ntaname) |>
-  dplyr::rename(neighborhood = ntaname)
+census_tract_to_cmnty_vec           <- census_tract_to_boro_nbd_data$community
+names(census_tract_to_cmnty_vec)    <- census_tract_to_boro_nbd_data$census_tract
 
-neighborhood_sf_obj <- sf::st_simplify(neighborhood_sf_obj,
-                                       preserveTopology = T,
-                                       dTolerance = 200)
+census_tract_to_boro_vec            <- census_tract_to_boro_nbd_data$borough
+names(census_tract_to_boro_vec)     <- census_tract_to_boro_nbd_data$census_tract
 
-community_sf_obj  <- sf::st_read("data-raw/nycdta2020_22b/") |>
-  dplyr::select(CDTA2020) |>
-  dplyr::rename(community = CDTA2020)
+nbd_to_cmnty_data                   <- census_tract_to_boro_nbd_data[,c("neighborhood", "community")] |>
+                                            unique(by = "neighborhood")
 
-community_sf_obj <- sf::st_simplify(community_sf_obj,
-                                    preserveTopology = T,
-                                    dTolerance = 200)
+nbd_to_cmnty_vec                    <- nbd_to_cmnty_data$community
+names(nbd_to_cmnty_vec)             <- nbd_to_cmnty_data$neighborhood
 
+nbd_to_boro_data                    <- census_tract_to_boro_nbd_data[,c("neighborhood", "borough")] |>
+                                            unique(by = "neighborhood")
+
+nbd_to_boro_vec                     <- nbd_to_boro_data$borough
+names(nbd_to_boro_vec)              <- nbd_to_boro_data$neighborhood
+
+cmnty_to_boro_data                  <- census_tract_to_boro_nbd_data[,c("community", "borough")] |>
+                                            unique(by = "community")
+
+cmnty_to_boro_vec                   <- cmnty_to_boro_data$borough
+names(cmnty_to_boro_vec)            <- cmnty_to_boro_data$community
+
+
+borough_sf_obj                      <- sf::st_read("data-raw/nybb_22b/nybb.shp") |>
+                                            dplyr::select(BoroName, geometry) |>
+                                            dplyr::rename(borough = BoroName)
+
+borough_sf_obj                      <- sf::st_simplify(
+                                                        borough_sf_obj,
+                                                        preserveTopology = T,
+                                                        dTolerance = 200
+                                                        )
+
+neighborhood_sf_obj               <- sf::st_read("data-raw/NYC_NTA_shp") |>
+                                            dplyr::select(ntaname) |>
+                                            dplyr::rename(neighborhood = ntaname)
+
+neighborhood_sf_obj               <- sf::st_simplify(
+                                                      neighborhood_sf_obj,
+                                                      preserveTopology = T,
+                                                      dTolerance = 200
+                                                      )
+
+community_sf_obj                  <- sf::st_read("data-raw/nycdta2020_22b/") |>
+                                            dplyr::select(CDTA2020) |>
+                                            dplyr::rename(community = CDTA2020)
+
+community_sf_obj                  <- sf::st_simplify(community_sf_obj,
+                                            preserveTopology = T,
+                                            dTolerance = 200)
 
 shape_list <- list()
 
-shape_list$censustract <- census_tract_sf_obj |>
-  dplyr::transmute(identifier = census_tract)
+shape_list$censustract            <- census_tract_sf_obj |>
+                                            dplyr::transmute(identifier = census_tract)
 
-shape_list$neighborhood <- neighborhood_sf_obj |>
-  dplyr::transmute(identifier = neighborhood)
+shape_list$neighborhood           <- neighborhood_sf_obj |>
+                                            dplyr::transmute(identifier = neighborhood)
 
-shape_list$community <- community_sf_obj |>
-  dplyr::transmute(identifier = community)
+shape_list$community              <- community_sf_obj |>
+                                            dplyr::transmute(identifier = community)
 
-shape_list$borough <- borough_sf_obj |>
-  dplyr::transmute(identifier = borough)
+shape_list$borough                <- borough_sf_obj |>
+                                            dplyr::transmute(identifier = borough)
 
-shape_list <-
-  lapply(shape_list, function(x)
-    sf::st_transform(x = x, crs = sf::st_crs(census_tract_sf_obj))) |>
-  lapply(data.frame)
+shape_list                        <- lapply(
+                                            shape_list,
+                                            function(x) sf::st_transform(x = x, crs = sf::st_crs(census_tract_sf_obj))
+                                            ) |>
+                                            lapply(data.frame)
 
 
+## COLORS
 
 mpxnyc_colors <- list()
 
@@ -112,10 +121,24 @@ mpxnyc_colors[["mid_brown"]]      <- "#D89000"
 mpxnyc_colors[["light_brown"]]    <- "#FFCD2E"
 mpxnyc_colors[["all_colors"]]     <- unlist(mpxnyc_colors)
 
-usethis::use_data(census_tract_sf_obj, census_tract_sf_obj,                                                       internal=FALSE, overwrite=TRUE)
-usethis::use_data(neighborhood_sf_obj, neighborhood_sf_obj,                                                       internal=FALSE, overwrite=TRUE)
-usethis::use_data(borough_sf_obj, borough_sf_obj,                                                                 internal=FALSE, overwrite=TRUE)
-usethis::use_data(mpxnyc_colors, mpxnyc_colors,                                                                   internal=FALSE, overwrite=TRUE)
+
+## DATA
+
+mpxnyc_1_person_data               <- readRDS("data-raw/mpxnyc_1_person_data.R")
+mpxnyc_1_place_data                <- readRDS("data-raw/mpxnyc_1_place_data.R")
+mpxnyc_2_data                      <- readRDS("data-raw/mpxnyc_2_data.R")
+
+
+
+# CREATE PACKAGE DATA
+
+usethis::use_data(census_tract_sf_obj,                                                      internal=FALSE, overwrite=TRUE)
+usethis::use_data(neighborhood_sf_obj,                                                      internal=FALSE, overwrite=TRUE)
+usethis::use_data(borough_sf_obj,                                                           internal=FALSE, overwrite=TRUE)
+usethis::use_data(mpxnyc_colors,                                                            internal=FALSE, overwrite=TRUE)
+usethis::use_data(mpxnyc_1_person_data,                                                     internal=FALSE, overwrite=TRUE)
+usethis::use_data(mpxnyc_1_place_data,                                                      internal=FALSE, overwrite=TRUE)
+usethis::use_data(mpxnyc_2_data,                                                            internal=FALSE, overwrite=TRUE)
 
 usethis::use_data(
                   census_tract_centroids,
